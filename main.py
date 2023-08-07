377cd2a03a1a2629f0642c691086e7d2b40b7619
teshnizi <teshnizi@stanford.edu>
2023-08-07 09:48:47 -0700

    minor

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def parse_args():
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=2e-3,
         help="the learning rate of the optimizer")
-    parser.add_argument("--num-envs", type=int, default=4,
+    parser.add_argument("--num-envs", type=int, default=8,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
